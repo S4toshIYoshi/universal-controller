e525853e001d5a4df5510c9f9bda40e4a605f3d7
S4toshIYoshi <n_krosh@mail.ru>
2023-08-22 13:08:25 +0300

    formating

--- input-controller-test.js
+++ input-controller-test.js
@@ -1,107 +1,101 @@
 import {InputController} from './input-controller.js'
 
 const ButtonKey = {
-    "left": {
-        keys: [65,37],
-        enabled: true
-    },
-    "right": {
-        keys: [68,39],
-        enabled: true
-    },
-    "top": {
-        keys: [87,38],
-        enabled: true
-    },
-    "bottom": {
-        keys: [83,40],
-        enabled: true
-    }
-
+  left: {
+    keys: [65, 37],
+    enabled: true,
+  },
+  right: {
+    keys: [68, 39],
+    enabled: true,
+  },
+  top: {
+    keys: [87, 38],
+    enabled: true,
+  },
+  bottom: {
+    keys: [83, 40],
+    enabled: true,
+  },
 }
 
 const jumpKey = {
-    "jump": {
-        keys: [32],
-        enabled: true
-    }
+  jump: {
+    keys: [32],
+    enabled: true,
+  },
 }
-
 
 const target = document.querySelector('.cube')
 
 const inputController = new InputController(ButtonKey, target)
 inputController.bindActions(jumpKey)
 
-
 const setting = {
-    x: 50,
-    y: 50,
-    speed: 1, // в процентах относительно экрана
-    color: 'black',
-    jump: {
-        hightJump: 1, // в процентах относительно экрана
-        cooldown: 1, //в секундах
-        timeInJump: 0.200, //в секундах
-        color: 'blue',
-        
-    }
+  x: 50,
+  y: 50,
+  speed: 1, // в процентах относительно экрана
+  color: 'black',
+  jump: {
+    hightJump: 1, // в процентах относительно экрана
+    cooldown: 1, //в секундах
+    timeInJump: 0.2, //в секундах
+    color: 'blue',
+  },
 }
 
 const attach = document.querySelector('.actived')
 attach.onclick = () => {
-    inputController.attach(target, false)
-    console.log('active')
+  inputController.attach(target, false)
+  console.log('active')
 }
 
 const detach = document.querySelector('.deactived')
 detach.onclick = () => {
-    inputController.detach()
-    console.log('deactive')
+  inputController.detach()
+  console.log('deactive')
 }
 
-
 const jump = () => {
-    setTimeout(() => {
-        setting.y += setting.jump.hightJump
-        target.style.top = `${setting.y}%`;
-        target.style.backgroundColor = setting.color
-        inputController.disableAction('jump')
-    }, setting.jump.timeInJump * 1000)
+  setTimeout(() => {
+    setting.y += setting.jump.hightJump
+    target.style.top = `${setting.y}%`
+    target.style.backgroundColor = setting.color
+    inputController.disableAction('jump')
+  }, setting.jump.timeInJump * 1000)
 
-    setTimeout(() => {
-        inputController.enableAction('jump')
-    }, setting.jump.cooldown * 1000)
+  setTimeout(() => {
+    inputController.enableAction('jump')
+  }, setting.jump.cooldown * 1000)
 
-    setting.y -= setting.jump.hightJump;
+  setting.y -= setting.jump.hightJump
 
-    target.style.top = `${setting.y}%`;
-    target.style.backgroundColor = setting.jump.color
+  target.style.top = `${setting.y}%`
+  target.style.backgroundColor = setting.jump.color
 }
 
 const move = () => {
-    if (inputController.isActionActive('right')) {
-        setting.x += setting.speed;
-        target.style.left = `${setting.x}%`;
-    }
-    if (inputController.isActionActive('left')) {
-        setting.x -= setting.speed;
-        target.style.left = `${setting.x}%`;
-    }
-    if (inputController.isActionActive('top')) {
-        setting.y -= setting.speed;
-        target.style.top = `${setting.y}%`;
-    }
-    if (inputController.isActionActive('bottom')) {
-        setting.y += setting.speed;
-        target.style.top = `${setting.y}%`;
-    }
+  if (inputController.isActionActive('right')) {
+    setting.x += setting.speed
+    target.style.left = `${setting.x}%`
+  }
+  if (inputController.isActionActive('left')) {
+    setting.x -= setting.speed
+    target.style.left = `${setting.x}%`
+  }
+  if (inputController.isActionActive('top')) {
+    setting.y -= setting.speed
+    target.style.top = `${setting.y}%`
+  }
+  if (inputController.isActionActive('bottom')) {
+    setting.y += setting.speed
+    target.style.top = `${setting.y}%`
+  }
 
-    if (inputController.isActionActive('jump')) {
-        jump()
-    }
-    window.requestAnimationFrame(move)
+  if (inputController.isActionActive('jump')) {
+    jump()
+  }
+  window.requestAnimationFrame(move)
 }
 
-window.requestAnimationFrame(move);
-
+window.requestAnimationFrame(move)
--- input-controller.js
+++ input-controller.js
@@ -1,98 +1,99 @@
 class KeyBoard {
-    constructor() {
-        this.pressButton = {}
-    }
+  constructor() {
+    this.pressButton = {}
+  }
 
-    event(e, press) {
-        for(let key in this.actionsToBind) {
-            if(this.actionsToBind[`${key}`].keys.indexOf(e.keyCode) != -1) {
-                this.actionsToBind[`${key}`].active = press
-                document.dispatchEvent(press ? this.actionActivated : this.actionDeactivated);
-            }
-        } 
+  event(e, press) {
+    for (let key in this.actionsToBind) {
+      if (this.actionsToBind[`${key}`].keys.indexOf(e.keyCode) != -1) {
+        this.actionsToBind[`${key}`].active = press
+        document.dispatchEvent(
+          press ? this.actionActivated : this.actionDeactivated
+        )
+      }
     }
+  }
 
-    upKey(e) {
-        delete this.pressButton[e.keyCode]
-        this.event(e, false)
-       
+  upKey(e) {
+    delete this.pressButton[e.keyCode]
+    this.event(e, false)
+  }
+
+  downKey(e) {
+    if (!this.isKeyPressed(e.keyCode)) {
+      this.pressButton[e.keyCode] = e.keyCode
     }
+    this.event(e, true)
+  }
 
-    downKey(e) {
-        if(!this.isKeyPressed(e.keyCode)){
-            this.pressButton[e.keyCode] = e.keyCode
-         }
-         this.event(e, true)
+  toggleListener(show) {
+    if (show) {
+      document.addEventListener('keydown', e => this.downKey(e))
+      document.addEventListener('keyup', e => this.upKey(e))
+    } else {
+      document.removeEventListener('keydown', e => this.downKey(e))
+      document.removeEventListener('keyup', e => this.upKey(e))
     }
+  }
 
-    toggleListener(show) {
-        if(show) {
-            document.addEventListener('keydown', e => this.downKey(e));
-            document.addEventListener('keyup', e => this.upKey(e));
-
-        } else {
-            document.removeEventListener('keydown', e => this.downKey(e));
-            document.removeEventListener('keyup', e => this.upKey(e));
-        }
-    }
-
-    isKeyPressed(keyCode) {
-        return this.pressButton.hasOwnProperty(keyCode);
-    }
-    
+  isKeyPressed(keyCode) {
+    return this.pressButton.hasOwnProperty(keyCode)
+  }
 }
 
+export class InputController extends KeyBoard {
+  enabled
+  focused
+  ACTION_ACTIVATED = 'input-controller:action-activated'
+  ACTION_DEACTIVATED = 'input-controller:action-deactivated'
 
-export class InputController extends KeyBoard{
-    enabled;
-    focused;
-    ACTION_ACTIVATED = "input-controller:action-activated";
-    ACTION_DEACTIVATED = "input-controller:action-deactivated";
+  constructor(actionsToBind, target) {
+    super(actionsToBind)
 
+    this.actionsToBind = actionsToBind
 
-    constructor(actionsToBind, target) {
-        super(actionsToBind)
+    this.target = target
+    this.enabled = false
 
-        this.actionsToBind = actionsToBind
+    this.actionActivated = new Event(this.ACTION_ACTIVATED)
+    this.actionDeactivated = new Event(this.ACTION_DEACTIVATED)
+  }
 
-        this.target = target
-        this.enabled = false
-        
-        this.actionActivated = new Event(this.ACTION_ACTIVATED)
-        this.actionDeactivated = new Event(this.ACTION_DEACTIVATED)
+  bindActions(actionsToBind) {
+    this.actionsToBind = Object.assign(this.actionsToBind, actionsToBind)
+  }
+
+  enableAction(actionName) {
+    if (this.enabled) {
+      this.actionsToBind[actionName].enabled = true
+      console.log(this.actionsToBind)
     }
+  }
 
-    bindActions(actionsToBind) {
-        this.actionsToBind = Object.assign(this.actionsToBind, actionsToBind)
+  disableAction(actionName) {
+    if (this.enabled) {
+      this.actionsToBind[actionName].enabled = false
     }
+  }
 
-    enableAction(actionName) {
-        if(this.enabled) {
-            this.actionsToBind[actionName].enabled = true
-            console.log(this.actionsToBind)
-        }
-            
-    }
+  attach(target, dontEnable) {
+    this.target = target
+    this.enabled = !!dontEnable ? false : true
+    this.toggleListener(true)
+  }
 
-    disableAction(actionName) {
-        if(this.enabled) {
-            this.actionsToBind[actionName].enabled = false
-        }       
-    }
+  detach() {
+    this.target = null
+    this.enabled = false
+    this.toggleListener(false)
+  }
 
-    attach(target, dontEnable) {
-       this.target = target
-       this.enabled = !!dontEnable ? false : true
-       this.toggleListener(true)
-    }
-
-    detach() {
-        this.target = null
-        this.enabled = false
-        this.toggleListener(false)
-    }
-
-    isActionActive(actionName) {
-        return this.enabled && this.actionsToBind[actionName].enabled && (this.actionsToBind[actionName].active || this.actionsToBind[actionName].keys.some(el => this.isKeyPressed(el)))
-    }
+  isActionActive(actionName) {
+    return (
+      this.enabled &&
+      this.actionsToBind[actionName].enabled &&
+      (this.actionsToBind[actionName].active ||
+        this.actionsToBind[actionName].keys.some(el => this.isKeyPressed(el)))
+    )
+  }
 }
