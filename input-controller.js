241926a88fd938a1d01650d1e88ab13caa5147bb
Dery456 <n_krosh@mail.ru>
2023-08-25 10:48:37 +0300

    fix bug

--- input-controller.js
+++ input-controller.js
@@ -1,373 +1,358 @@
 export class BasePlugin {
-  constructor(actionsToBind) {
-    this.actionsToBind = actionsToBind // бинды
+	constructor(actionsToBind) {
+		this.actionsToBind = actionsToBind; // бинды
 
-    this.succsesKey = false
+		this.succsesKey = false;
 
-    this.actionActivated = [] // кеолекция активных экшенов
-    this.actionDeactivated = [] // колекция деактивных экшенов
-  }
+		this.actionActivated = []; // кеолекция активных экшенов
+		this.actionDeactivated = []; // колекция деактивных экшенов
+	}
 
-  /**
-   * @description инициализирует активные экшены
-   * @param  {object} events
-   */
-  initEventsActive(...events) {
-    this.actionActivated.push(...events)
-  }
+	/**
+	 * @description инициализирует активные экшены
+	 * @param  {object} events
+	 */
+	initEventsActive(...events) {
+		this.actionActivated.push(...events);
+	}
 
-  /**
-   * @description инициализирует не активные экшены
-   * @param  {object} events
-   */
-  initEventsDeactive(...events) {
-    this.actionDeactivated.push(...events)
-  }
+	/**
+	 * @description инициализирует не активные экшены
+	 * @param  {object} events
+	 */
+	initEventsDeactive(...events) {
+		this.actionDeactivated.push(...events);
+	}
 
-  /**
-   * @description проверяет есть ли в нажатых кнопках похожие
-   * @param {string} action
-   * @returns {bool}
-   */
-  actionActive(action) {
-    if (this.actionsToBind[action]) {
-      return this.actionsToBind[action].allkeys.some(el =>
-        this.actionActivated[0].detail.pressButton.hasOwnProperty(el)
-      )
-    }
-  }
+	setDetachAction() {
+		this.actionActivated = [];
+		this.actionDeactivated = [];
+	}
 
-  /**
-   * @description обновляет set экшенов
-   * @param  {object} activity
-   * @param  {object} deactivity
-   */
-  updateActivity(activity, deactivity) {
-    this.activity = activity
-    this.deactivity = deactivity
-  }
-  /**
-   * @description обновляет список всех биндов
-   * @param {object} newBind
-   */
-  updateMap(newBind) {
-    this.allBindKey = new Map([...newBind])
-  }
+	/**
+	 * @description проверяет есть ли в нажатых кнопках похожие
+	 * @param {string} action
+	 * @returns {bool}
+	 */
+	actionActive(action) {
+		if (this.actionsToBind[action]) {
+			return this.actionsToBind[action].allkeys.some(el =>
+				this.actionActivated[0].detail.pressButton.hasOwnProperty(el)
+			);
+		}
+	}
 
-  /**
-   * @description обновляет Map всех нажатых кнопок
-   * @param  {object} newBind
-   */
+	/**
+	 * @description обновляет set экшенов
+	 * @param  {object} activity
+	 * @param  {object} deactivity
+	 */
+	updateActivity(activity, deactivity) {
+		this.activity = activity;
+		this.deactivity = deactivity;
+	}
+	/**
+	 * @description обновляет список всех биндов
+	 * @param {object} newBind
+	 */
+	updateMap(newBind) {
+		this.allBindKey = new Map([...newBind]);
+	}
 
-  /**
-   * @description првоверят есть ли переданное значение во всех биндах
-   * @param  {number} keyCode
-   */
-  searchKey(keyCode) {
-    if (this.allBindKey.has(keyCode)) {
-      this.succsesKey = true
-    } else {
-      this.succsesKey = false
-    }
-  }
+	/**
+	 * @description обновляет Map всех нажатых кнопок
+	 * @param  {object} newBind
+	 */
 
-  /**
-   * @description проверяет нажата ли кнопка по переданомму коду
-   * @param  {number} keyCode
-   */
-  isButtonsActive(keyCode) {
-    return (
-      this.actionActivated[0].detail.pressButton.hasOwnProperty(keyCode) &&
-      !!this.allBindKey.get(keyCode)
-    )
-  }
+	/**
+	 * @description првоверят есть ли переданное значение во всех биндах
+	 * @param  {number} keyCode
+	 */
+	searchKey(keyCode) {
+		if (this.allBindKey.has(keyCode)) {
+			this.succsesKey = true;
+		} else {
+			this.succsesKey = false;
+		}
+	}
 
-  /**
-   * @description type -  *тип который должен быть в ивенте чтобы событие задиспатчилось*
-   * @description activated -  *по каким ивентам искать: true - активирующим, false - деактивирующим**
-   * @description conditions -  *условие при которм выполниться поиск по ивентам, по умолчанию true*
-   * @param {string} type
-   * @param {bool} activated
-   * @param {bool} conditions
-   *
-   */
-  generationDispath(type, activated, conditions = true) {
-    if (conditions) {
-      if (activated) {
-        this.actionActivated.forEach(el => {
-          if (el.detail.type === type) {
-            console.log('active')
-            document.dispatchEvent(el)
-          }
-        })
-      } else {
-        this.actionDeactivated.forEach(el => {
-          if (el.detail.type === type) {
-            console.log('deactive')
-            document.dispatchEvent(el)
-          }
-        })
-      }
-    }
-  }
+	/**
+	 * @description проверяет нажата ли кнопка по переданомму коду
+	 * @param  {number} keyCode
+	 */
+	isButtonsActive(keyCode) {
+		return (
+			this.actionActivated[0].detail.pressButton.hasOwnProperty(keyCode) &&
+			!!this.allBindKey.get(keyCode)
+		);
+	}
 
-  /**
-   * @description цепляет к document слушатель на переданное событие
-   * @param {string} eventType
-   * @param {Function} handler
-   * @param {bool} show
-   *
-   */
+	/**
+	 * @description type -  *тип который должен быть в ивенте чтобы событие задиспатчилось*
+	 * @description activated -  *по каким ивентам искать: true - активирующим, false - деактивирующим**
+	 * @description conditions -  *условие при которм выполниться поиск по ивентам, по умолчанию true*
+	 * @param {string} type
+	 * @param {bool} activated
+	 * @param {bool} conditions
+	 *
+	 */
+	generationDispath(type, activated, conditions = true) {
+		if (conditions) {
+			if (activated) {
+				this.actionActivated.forEach(el => {
+					if (el.detail.type === type) {
+						console.log('active');
+						document.dispatchEvent(el);
+					}
+				});
+			} else {
+				this.actionDeactivated.forEach(el => {
+					if (el.detail.type === type) {
+						console.log('deactive');
+						document.dispatchEvent(el);
+					}
+				});
+			}
+		}
+	}
 
-  generationListener(eventType, handler, show) {
-    if (show) {
-      document.addEventListener(eventType, handler)
-    } else {
-      document.removeEventListener(eventType, handler)
-    }
-  }
+	/**
+	 * @description цепляет к document слушатель на переданное событие
+	 * @param {string} eventType
+	 * @param {Function} handler
+	 * @param {bool} show
+	 *
+	 */
+
+	generationListener(eventType, handler, show) {
+		if (show) {
+			document.addEventListener(eventType, handler);
+		} else {
+			document.removeEventListener(eventType, handler);
+		}
+	}
 }
 
 export class InputController {
-  enabled // доступность контроллера
-  focused // фокус пользователя на экран
+	enabled; // доступность контроллера
+	focused; // фокус пользователя на экран
 
-  // Экшены
-  ACTION_ACTIVATED = 'input-controller:action-activated'
-  ACTION_DEACTIVATED = 'input-controller:action-deactivated'
-  ACTION_ADVERTISING_DEACTIVATED = 'input-controller:advertising-deactevated'
+	// Экшены
+	ACTION_ACTIVATED = 'input-controller:action-activated';
+	ACTION_DEACTIVATED = 'input-controller:action-deactivated';
 
-  plugins // все подключенные плагины
+	plugins; // все подключенные плагины
 
-  target // объект к которму цепляемся
-  activity // Set колекция с активными экшенами
-  deactivity // Set колекция с не активными экшенами
+	target; // объект к которму цепляемся
+	activity; // Set колекция с активными экшенами
+	deactivity; // Set колекция с не активными экшенами
 
-  constructor(actionsToBind, target = null) {
-    this.actionsToBind = actionsToBind
+	constructor(actionsToBind, target = null) {
+		this.actionsToBind = actionsToBind;
 
-    this.plugins = []
+		this.plugins = [];
 
-    this.target = target
-    this.enabled = false
+		this.target = target;
+		this.enabled = false;
 
-    this.actionActivated = new CustomEvent(this.ACTION_ACTIVATED, {
-      detail: {
-        type: 'click',
-        key: null,
-        active: false,
-        action: new Set(),
-        pressButton: {},
-      },
-    })
-    this.actionDeactivated = new CustomEvent(this.ACTION_DEACTIVATED, {
-      detail: {
-        type: 'click',
-        key: null,
-        active: false,
-        action: new Set(),
-      },
-    })
+		this.actionActivated = new CustomEvent(this.ACTION_ACTIVATED, {
+			detail: {
+				type: 'click',
+				key: null,
+				active: false,
+				action: new Set(),
+				pressButton: {},
+			},
+		});
+		this.actionDeactivated = new CustomEvent(this.ACTION_DEACTIVATED, {
+			detail: {
+				type: 'click',
+				key: null,
+				active: false,
+				action: new Set(),
+			},
+		});
 
-    this.actionAdvertising = new CustomEvent(
-      this.ACTION_ADVERTISING_DEACTIVATED,
-      {
-        detail: {
-          type: 'click',
-          key: null,
-          active: false,
-          action: new Set(),
-        },
-      }
-    )
+		this.activity = new Set();
+		this.deactivity = new Set();
 
-    this.activity = new Set()
-    this.deactivity = new Set()
+		this.allBindKey = new Map();
+		this.filingMap(this.actionsToBind);
 
-    this.allBindKey = new Map()
-    this.filingMap(this.actionsToBind)
+		/**
+		 *
+		 * @description срабатывает когда экшен активен
+		 * @description обновляет колекцию deactivity и detail.action
+		 */
+		this.handlerActivity = e => {
+			this.deactivity.clear();
+			this.updateSet();
+		};
+		/**
+		 *
+		 * @description срабатывает когда экшен активен
+		 * @description обновляет колекцию activity и detail.action
+		 */
+		this.handlerDeactivity = e => {
+			this.activity.clear();
 
-    /**
-     *
-     * @description срабатывает когда экшен активен
-     * @description обновляет колекцию deactivity и detail.action
-     */
-    this.handlerActivity = e => {
-      this.deactivity.clear()
-      this.updateSet()
-    }
-    /**
-     *
-     * @description срабатывает когда экшен активен
-     * @description обновляет колекцию activity и detail.action
-     */
-    this.handlerDeactivity = e => {
-      this.activity.clear()
+			this.updateSet();
+		};
+	}
 
-      this.updateSet()
-    }
-  }
+	/**
+	 *
+	 * @description добовляет новые бинды и раскидывает их по плагинам
+	 */
+	bindActions(actionsToBind) {
+		this.actionsToBind = Object.assign(this.actionsToBind, actionsToBind);
+		this.allBindKey.clear();
+		this.filingMap(this.actionsToBind);
+		this.plugins.forEach(el => {
+			el.updateMap(this.allBindKey);
+		});
+	}
 
-  /**
-   *
-   * @description добовляет новые бинды и раскидывает их по плагинам
-   */
-  bindActions(actionsToBind) {
-    this.actionsToBind = Object.assign(this.actionsToBind, actionsToBind)
-    this.allBindKey.clear()
-    this.filingMap(this.actionsToBind)
-    this.plugins.forEach(el => {
-      el.updateMap(this.allBindKey)
-    })
-  }
+	/**
+	 * @description заполняет Map со всеми биндами key => name
+	 * @param {object} bind
+	 */
+	filingMap(bind) {
+		if (bind) {
+			for (let key in bind) {
+				bind[key].allkeys.forEach(el => this.allBindKey.set(el, key));
+			}
+		}
+	}
 
-  /**
-   * @description заполняет Map со всеми биндами key => name
-   * @param {object} bind
-   */
-  filingMap(bind) {
-    if (bind) {
-      for (let key in bind) {
-        bind[key].allkeys.forEach(el => this.allBindKey.set(el, key))
-      }
-    }
-  }
+	enableAction(actionName) {
+		if (this.enabled && this.target) {
+			this.actionsToBind[actionName].enabled = true;
+		}
+	}
 
-  enableAction(actionName) {
-    if (this.enabled && this.target) {
-      this.actionsToBind[actionName].enabled = true
-    }
-  }
+	disableAction(actionName) {
+		if (this.enabled && this.target) {
+			this.actionsToBind[actionName].enabled = false;
+		}
+	}
 
-  disableAction(actionName) {
-    if (this.enabled && this.target) {
-      this.actionsToBind[actionName].enabled = false
-    }
-  }
+	attach(target, dontEnable) {
+		this.target = target;
+		this.enabled = !!dontEnable ? false : true;
 
-  attach(target, dontEnable) {
-    this.target = target
-    this.enabled = !!dontEnable ? false : true
+		this.plugins.forEach(el => {
+			el.initEventsActive(this.actionActivated);
+			el.initEventsDeactive(this.actionDeactivated);
+			el.listener(true);
+			el.updateMap(this.allBindKey);
+		});
 
-    this.plugins.forEach(el => {
-      el.initEventsActive(this.actionActivated)
-      el.initEventsDeactive(this.actionDeactivated, this.actionAdvertising)
-      el.listener(true)
-      el.updateMap(this.allBindKey)
-    })
+		document.addEventListener(this.ACTION_ACTIVATED, this.handlerActivity);
+		document.addEventListener(this.ACTION_DEACTIVATED, this.handlerDeactivity);
+	}
 
-    document.addEventListener(this.ACTION_ACTIVATED, this.handlerActivity)
-    document.addEventListener(this.ACTION_DEACTIVATED, this.handlerDeactivity)
-    document.addEventListener(
-      this.ACTION_ADVERTISING_DEACTIVATED,
-      this.handlerDeactivity
-    )
-  }
+	detach() {
+		this.plugins.forEach(el => {
+			el.listener(false);
+			el.setDetachAction();
+		});
+		this.target = null;
+		this.enabled = false;
 
-  detach() {
-    this.plugins.forEach(el => {
-      el.listener(false)
-    })
-    this.target = null
-    this.enabled = false
+		document.removeEventListener(this.ACTION_ACTIVATED, this.handlerActivity);
+		document.removeEventListener(
+			this.ACTION_DEACTIVATED,
+			this.handlerDeactivity
+		);
+	}
 
-    document.removeEventListener(this.ACTION_ACTIVATED, this.handlerActivity)
-    document.removeEventListener(
-      this.ACTION_DEACTIVATED,
-      this.handlerDeactivity
-    )
-    document.removeEventListener(
-      this.ACTION_ADVERTISING_DEACTIVATED,
-      this.handlerDeactivity
-    )
-  }
+	isActionActive(actionName) {
+		const isBool =
+			this.focused &&
+			this.enabled &&
+			this.target &&
+			this.actionsToBind[actionName].enabled &&
+			this.isPresent(this.actionsToBind[actionName], ['keys', 'mouse']);
 
-  isActionActive(actionName) {
-    const isBool =
-      this.focused &&
-      this.enabled &&
-      this.target &&
-      this.actionsToBind[actionName].enabled &&
-      this.isPresent(this.actionsToBind[actionName], ['keys', 'mouse'])
+		return isBool;
+	}
 
-    return isBool
-  }
+	isKeyPressed(keyCode) {
+		return this.plugins.some(el => {
+			return el.isButtonsActive(keyCode);
+		});
+	}
 
-  isKeyPressed(keyCode) {
-    return this.plugins.some(el => {
-      return el.isButtonsActive(keyCode)
-    })
-  }
+	/**
+	 *
+	 * @description сеттер для проверки фокуса
+	 */
+	isFocus() {
+		this.focused = document.hasFocus();
 
-  /**
-   *
-   * @description сеттер для проверки фокуса
-   */
-  isFocus() {
-    this.focused = document.hasFocus()
+		if (!this.focused) {
+			this.focused = false;
+		} else {
+			this.focused = true;
+		}
 
-    if (!this.focused) {
-      this.focused = false
-    } else {
-      this.focused = true
-    }
+		return this.focused;
+	}
 
-    return this.focused
-  }
+	/**
+	 *
+	 * @description инициирует переданные плагины
+	 */
+	registerPlugin(...arg) {
+		this.plugins.push(...arg);
+		console.log(this.plugins);
+	}
 
-  /**
-   *
-   * @description инициирует переданные плагины
-   */
-  registerPlugin(...arg) {
-    this.plugins.push(...arg)
-    console.log(this.plugins)
-  }
+	/**
+	 *
+	 * @description обновляет списки активности и деактивности, затем отправляет их в плагины
+	 */
+	updateSet() {
+		this.plugins.reduce((acc, el) => {
+			if (el.activity && el.deactivity) {
+				this.activity = new Set([
+					...this.activity,
+					...el.activity,
+					...acc.activity,
+				]);
+				this.deactivity = new Set([
+					...el.deactivity,
+					...acc.deactivity,
+					...this.deactivity,
+				]);
+			}
+		});
 
-  /**
-   *
-   * @description обновляет списки активности и деактивности, затем отправляет их в плагины
-   */
-  updateSet() {
-    this.plugins.reduce((acc, el) => {
-      if (el.activity && el.deactivity) {
-        this.activity = new Set([
-          ...this.activity,
-          ...el.activity,
-          ...acc.activity,
-        ])
-        this.deactivity = new Set([
-          ...el.deactivity,
-          ...acc.deactivity,
-          ...this.deactivity,
-        ])
-      }
-    })
+		this.plugins.forEach(el =>
+			el.updateActivity(this.activity, this.deactivity)
+		);
+	}
 
-    this.plugins.forEach(el =>
-      el.updateActivity(this.activity, this.deactivity)
-    )
-  }
+	/**
+	 *
+	 * @description проверят есть ли в объектк личный ключ
+	 */
+	isKey(obj, key) {
+		return typeof obj[key] === 'object';
+	}
 
-  /**
-   *
-   * @description проверят есть ли в объектк личный ключ
-   */
-  isKey(obj, key) {
-    return typeof obj[key] === 'object'
-  }
+	/**
+	 *
+	 * @description проверяет присутсвует ли действия в в нажатых клавишах
+	 */
+	isPresent(obj, key = []) {
+		const result = key.map(key => {
+			if (this.isKey(obj, key)) {
+				return obj[key].some(el => this.isKeyPressed(el));
+			}
+		});
 
-  /**
-   *
-   * @description проверяет присутсвует ли действия в в нажатых клавишах
-   */
-  isPresent(obj, key = []) {
-    const result = key.map(key => {
-      if (this.isKey(obj, key)) {
-        return obj[key].some(el => this.isKeyPressed(el))
-      }
-    })
-
-    return result.some(el => el)
-  }
+		return result.some(el => el);
+	}
 }
